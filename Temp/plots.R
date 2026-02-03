df8d1363b9318734e17d6d1148ed4a379b4e868b
Francisco Javier Rubio <fxrubio@gmail.com>
2026-02-03 13:05:40 +0000

    Update plots.R

--- Temp/plots.R
+++ Temp/plots.R
@@ -1,20 +1,17 @@
+rm(list = ls())
 library(twopiece)
 library(numDeriv)
 
-source("routines_tp.R")
+source("C:/Users/Javier/Documents/GitHub/MOOMIN/routines/routines_tp.R")
 
-curve(sdiscrepancy_min_tpn,-3,3)
+eps <- seq(-3,3,by = 0.1)
 
-curve(unprior_min_tpn,-3,3)
+stpn <- sdiscrepancy_min_tpn(eps)
+ptpn <- unprior_min_tpn(eps)
 
+stplogis <- sdiscrepancy_min_tplogis(eps)
+ptplpgis <- unprior_min_tplogis(eps)
 
+stplap <- sdiscrepancy_min_tplap(eps)
+ptplap <- unprior_min_tplap(eps)
 
-
-curve(sdiscrepancy_min_logis,-3,3)
-
-curve(unprior_min_tpn,-3,3)
-
-
-curve(sdiscrepancy_min_lap,-3,3)
-
-curve(unprior_min_lap,-3,3)
