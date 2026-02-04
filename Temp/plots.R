90264b46de9d55bed06fea24c4b5793c44adebeb
Francisco Javier Rubio <fxrubio@gmail.com>
2026-02-04 14:47:20 +0000

    Expand integrate range, use optim, mirror eps
    Increase integration limits in tplogis discrepancy routines from +/-15 to +/-75 to better capture tails. Replace nlminb with optim and use its $value for the minimization result. Update plotting script to generate a negative-only eps grid (30 points), mirror values to create a full symmetric grid, and plot using the combined arrays; also add cleanup and a specific source path. These changes improve numerical stability and produce symmetric plots for the two-piece distributions.

--- Temp/plots.R
+++ Temp/plots.R
@@ -1,58 +1,119 @@
 rm(list = ls())
+
+# Required packages
 library(twopiece)
 library(numDeriv)
 
+# Routines
 source("C:/Users/Javier/Documents/GitHub/MOOMIN/routines/routines_tp.R")
 #source("~/Documents/GitHub/MOOMIN/routines/routines_tp.R")
 
-eps <- round(sort(c(0,seq(-3,3,length.out = 100))), digits = 2)
+################################################################################
+# Grid of points on (-3,0) for evaluations
+################################################################################
+eps <- round(seq(-3,0,length.out = 30), digits = 2)
 
+################################################################################
+# Evaluations
+################################################################################
+
+# twopiece normal
 disctpn <- discrepancy_min_tpn(eps)
-stpn <- sdiscrepancy_min_tpn(eps)
 ptpn <- unprior_min_tpn(eps)
 
+# twopiece logistic
 disctplogis <- discrepancy_min_tplogis(eps)
-stplogis <- sdiscrepancy_min_tplogis(eps)
-ptplpgis <- unprior_min_tplogis(eps)
+ptplogis <- unprior_min_tplogis(eps)
 
+# twopiece Laplace
 disctplap <- discrepancy_min_tplap(eps)
-stplap <- sdiscrepancy_min_tplap(eps)
 ptplap <- unprior_min_tplap(eps)
 
+################################################################################
+# Data preparation
+################################################################################
 
+# Grid
+eps_neg  <- eps
+eps_pos  <- -rev(eps_neg[-length(eps_neg)])
 
+# twopiece normal
+disctpn_neg <- disctpn
+disctpn_pos <-  rev(disctpn_neg[-length(disctpn_neg)])
+
+sdisctpn_neg <- -disctpn
+sdisctpn_pos <-  -rev(sdisctpn_neg[-length(sdisctpn_neg)])
+
+ptpn_neg <- ptpn
+ptpn_pos <-  rev(ptpn[-length(ptpn)])
+
+# twopiece logistic
+disctplogis_neg <- disctplogis
+disctplogis_pos <-  rev(disctplogis_neg[-length(disctplogis_neg)])
+
+sdisctplogis_neg <- -disctplogis
+sdisctplogis_pos <-  -rev(sdisctplogis_neg[-length(sdisctplogis_neg)])
+
+ptplogis_neg <- ptplogis
+ptplogis_pos <-  rev(ptplogis[-length(ptplogis)])
+
+# twopiece Laplace
+disctplap_neg <- disctplap
+disctplap_pos <-  rev(disctplap_neg[-length(disctplap_neg)])
+
+sdisctplap_neg <- -disctplap
+sdisctplap_pos <-  -rev(sdisctplap_neg[-length(sdisctplap_neg)])
+
+ptplap_neg <- ptplap
+ptplap_pos <-  rev(ptplap[-length(ptplap)])
+
+# full grid and values
+eps_full  <- c(eps_neg, eps_pos)
+
+disctpn_full <- c(disctpn_neg, disctpn_pos)
+sdisctpn_full <- c(sdisctpn_neg, sdisctpn_pos)
+ptpn_full <- c(ptpn_neg, ptpn_pos)
+
+disctplogis_full <- c(disctplogis_neg, disctplogis_pos)
+sdisctplogis_full <- c(sdisctplogis_neg, sdisctplogis_pos)
+ptplogis_full <- c(ptplogis_neg, ptplogis_pos)
+
+################################################################################
+# Plots
+################################################################################
+
+# twopiece normal
 pdf("disc_min_tpn.pdf", width = 8, height = 6)
-plot(eps,disctpn, lwd = 2, main = "two-piece normal", type = "l",
+plot(eps_full,disctpn_full, lwd = 2, main = "two-piece normal", type = "l",
       xlab = expression(epsilon), ylab = "Discrepancy", cex.axis = 1.5, cex.lab = 1.5)
 dev.off()
 pdf("sdisc_min_tpn.pdf", width = 8, height = 6)
-plot(eps,stpn, lwd = 2, main = "two-piece normal", type = "l",
+plot(eps_full,sdisctpn_full, lwd = 2, main = "two-piece normal", type = "l",
      xlab = expression(epsilon), ylab = "Signed discrepancy", cex.axis = 1.5, cex.lab = 1.5)
 dev.off()
 pdf("prior_min_tpn.pdf", width = 8, height = 6)
-plot(eps,ptpn, lwd = 2, main = "two-piece normal", type = "l",
+plot(eps_full,ptpn_full, lwd = 2, main = "two-piece normal", type = "l",
      xlab = expression(epsilon), ylab = "Prior", cex.axis = 1.5, cex.lab = 1.5)
 dev.off()
 
 
 
-
-
+# twopiece logistic
 pdf("disc_min_tplogis.pdf", width = 8, height = 6)
-plot(eps,disctplogis, lwd = 2, main = "two-piece logistic", type = "l",
+plot(eps_full,disctplogis_full, lwd = 2, main = "two-piece logistic", type = "l",
      xlab = expression(epsilon), ylab = "Discrepancy", cex.axis = 1.5, cex.lab = 1.5)
 dev.off()
 pdf("sdisc_min_tplogis.pdf", width = 8, height = 6)
-plot(eps,stplogis, lwd = 2, main = "two-piece logistic", type = "l",
+plot(eps_full,sdisctplogis_full, lwd = 2, main = "two-piece logistic", type = "l",
      xlab = expression(epsilon), ylab = "Signed discrepancy", cex.axis = 1.5, cex.lab = 1.5)
 dev.off()
 pdf("prior_min_tplogis.pdf", width = 8, height = 6)
-plot(eps,ptplogis, lwd = 2, main = "two-piece logistic", type = "l",
+plot(eps_full,ptplogis_full, lwd = 2, main = "two-piece logistic", type = "l",
      xlab = expression(epsilon), ylab = "Prior", cex.axis = 1.5, cex.lab = 1.5)
 dev.off()
 
 
-
+# twopiece Laplace
 pdf("disc_min_tplap.pdf", width = 8, height = 6)
 plot(eps,disctplap, lwd = 2, main = "two-piece Laplace", type = "l",
      xlab = expression(epsilon), ylab = "Discrepancy", cex.axis = 1.5, cex.lab = 1.5)
--- routines/routines_tp.R
+++ routines/routines_tp.R
@@ -33,41 +33,6 @@ discrepancy_min_tpn <- Vectorize(function(eps){
 
 
 ################################################################################
-# Signed Minimum Discrepancy measure: normal(mu,sigma) vs twopiece normal
-# eps: real number, skewness parameter
-################################################################################
-
-sdiscrepancy_min_tpn <- Vectorize(function(eps){
-  # Absolute value using the symmetry of the discrepancy measure
-  sgn <- sign(eps)
-  eps <- tanh(eps)
-  eps = abs(eps)
-  dtp <- Vectorize(function(x) dtp3(x,0,1,eps, param = "eps", FUN = dnorm))
-  # Discrepancy
-  disc <- function(par){
-    # Integrand
-    tempf <- Vectorize(function(x){
-      num <- dnorm(x,par[1],exp(par[2]))^2
-      den <- dnorm(x,par[1],exp(par[2])) + dtp(x)
-      out <- num/den
-      return(out)
-    })
-    # Integral
-    int <- integrate(tempf,-15,0)$value +  integrate(tempf,0,15)$value
-    return(int)
-  }
-  
-  # Initial value (mean and sd)
-  sim <- rtp3(10000,0,1,eps, param = "eps", FUN = rnorm)
-  init <- c(mean(sim),sd(sim))
-  
-  # Minimum translated signed discrepancy
-  val <- nlminb(init,disc)$objective-0.5
-  return(sgn*abs(val))
-})
-
-
-################################################################################
 # MOOMIN Prior on the skewness parameter of the twopiece normal
 # Based on assuming a uniform prior on the signed minimum discrepancy measure
 ################################################################################
@@ -109,7 +74,7 @@ discrepancy_min_tplogis <- Vectorize(function(eps){
       return(out)
     })
     # Integral
-    int <- integrate(tempf,-15,0)$value +  integrate(tempf,0,15)$value
+    int <- integrate(tempf,-75,0)$value +  integrate(tempf,0,75)$value
     return(int)
   }
   
@@ -118,44 +83,8 @@ discrepancy_min_tplogis <- Vectorize(function(eps){
   init <- c(mean(sim),sd(sim))
   
   # Minimum translated signed discrepancy
-  val <- nlminb(init,disc)$objective-0.5
+  val <- optim(init,disc)$value-0.5
   return(abs(val))
-})
-
-
-
-################################################################################
-# Signed Minimum Discrepancy measure: logistic(mu,sigma) vs twopiece logistic
-# eps: real number, skewness parameter
-################################################################################
-
-sdiscrepancy_min_tplogis <- Vectorize(function(eps){
-  # Absolute value using the symmetry of the discrepancy measure
-  sgn <- sign(eps)
-  eps <- tanh(eps)
-  eps = abs(eps)
-  dtp <- Vectorize(function(x) dtp3(x,0,1,eps, param = "eps", FUN = dlogis))
-  # Discrepancy
-  disc <- function(par){
-    # Integrand
-    tempf <- Vectorize(function(x){
-      num <- dlogis(x,par[1],exp(par[2]))^2
-      den <- dlogis(x,par[1],exp(par[2])) + dtp(x)
-      out <- num/den
-      return(out)
-    })
-    # Integral
-    int <- integrate(tempf,-15,0)$value +  integrate(tempf,0,15)$value
-    return(int)
-  }
-  
-  # Initial value (mean and sd)
-  sim <- rtp3(10000,0,1,eps, param = "eps", FUN = rlogis)
-  init <- c(mean(sim),sd(sim))
-  
-  # Minimum translated signed discrepancy
-  val <- nlminb(init,disc)$objective-0.5
-  return(sgn*abs(val))
 })
 
 
@@ -169,11 +98,19 @@ sdiscrepancy_min_tplogis <- Vectorize(function(eps){
 # Un-normalised prior
 # Based on numerical integration of the expression obtained in a Proposition
 unprior_min_tplogis <- Vectorize(function(eps){
-  eps = abs(eps)
+  if(eps==0) prior = 0
+  if(eps!=0){
+    eps = abs(eps)
   # prior
-  prior <- grad(sdiscrepancy_min_tplogis, x = eps,  method.args=list(eps=1e-8) )
+  prior <- grad(discrepancy_min_tplogis, x = eps,  method.args=list(eps=1e-8) )
+  }
   return(abs(prior))
 })
+
+
+
+
+
 
 
 # Probability Density Function: location parameter mu and scale parameter sigma
@@ -223,39 +160,6 @@ discrepancy_min_tplap <- Vectorize(function(eps){
   return(abs(val))
 })
 
-################################################################################
-# Signed Minimum Discrepancy measure: Laplace(mu,sigma) vs twopiece Laplace
-# eps: real number, skewness parameter
-################################################################################
-
-sdiscrepancy_min_tplap <- Vectorize(function(eps){
-  # Absolute value using the symmetry of the discrepancy measure
-  sgn <- sign(eps)
-  eps <- tanh(eps)
-  eps = abs(eps)
-  dtp <- Vectorize(function(x) dtp3(x,0,1,eps, param = "eps", FUN = dlap))
-  # Discrepancy
-  disc <- function(par){
-    # Integrand
-    tempf <- Vectorize(function(x){
-      num <- dlap(x,par[1],exp(par[2]))^2
-      den <- dlap(x,par[1],exp(par[2])) + dtp(x)
-      out <- num/den
-      return(out)
-    })
-    # Integral
-    int <- integrate(tempf,-60,0)$value +  integrate(tempf,0,60)$value
-    return(int)
-  }
-  
-  # Initial value (mean and sd)
-  sim <- rtp3(10000,0,1,eps, param = "eps", FUN = rlap)
-  init <- c(mean(sim),sd(sim))
-  
-  # Minimum translated signed discrepancy
-  val <- nlminb(init,disc)$objective-0.5
-  return(sgn*abs(val))
-})
 
 ################################################################################
 # MOOMIN Prior on the skewness parameter of the twopiece Laplace
@@ -267,9 +171,12 @@ sdiscrepancy_min_tplap <- Vectorize(function(eps){
 # Un-normalised prior
 # Based on numerical integration of the expression obtained in a Proposition
 unprior_min_tplap <- Vectorize(function(eps){
-  eps = abs(eps)
+  if(eps==0) prior = 0
+  if(eps!=0){
+    eps = abs(eps)
   # prior
   prior <- grad(sdiscrepancy_min_tplap, x = eps,  method.args=list(eps=1e-8) )
+  }
   return(abs(prior))
 })
 
