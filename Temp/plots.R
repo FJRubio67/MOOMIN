b5a8a525f015814ac325e040761bf88473438722
Francisco Javier Rubio <fxrubio@gmail.com>
2026-02-04 11:48:40 +0000

    Update plots.R

--- Temp/plots.R
+++ Temp/plots.R
@@ -5,7 +5,7 @@ library(numDeriv)
 source("C:/Users/Javier/Documents/GitHub/MOOMIN/routines/routines_tp.R")
 #source("~/Documents/GitHub/MOOMIN/routines/routines_tp.R")
 
-eps <- round(sort(c(0,seq(-3,3,length.out = 300))), digits = 2)
+eps <- round(sort(c(0,seq(-3,3,length.out = 100))), digits = 2)
 
 disctpn <- discrepancy_min_tpn(eps)
 stpn <- sdiscrepancy_min_tpn(eps)
@@ -31,5 +31,37 @@ plot(eps,stpn, lwd = 2, main = "two-piece normal", type = "l",
 dev.off()
 pdf("prior_min_tpn.pdf", width = 8, height = 6)
 plot(eps,ptpn, lwd = 2, main = "two-piece normal", type = "l",
+     xlab = expression(epsilon), ylab = "Prior", cex.axis = 1.5, cex.lab = 1.5)
+dev.off()
+
+
+
+
+
+pdf("disc_min_tplogis.pdf", width = 8, height = 6)
+plot(eps,disctplogis, lwd = 2, main = "two-piece logistic", type = "l",
+     xlab = expression(epsilon), ylab = "Discrepancy", cex.axis = 1.5, cex.lab = 1.5)
+dev.off()
+pdf("sdisc_min_tplogis.pdf", width = 8, height = 6)
+plot(eps,stplogis, lwd = 2, main = "two-piece logistic", type = "l",
+     xlab = expression(epsilon), ylab = "Signed discrepancy", cex.axis = 1.5, cex.lab = 1.5)
+dev.off()
+pdf("prior_min_tplogis.pdf", width = 8, height = 6)
+plot(eps,ptplogis, lwd = 2, main = "two-piece logistic", type = "l",
+     xlab = expression(epsilon), ylab = "Prior", cex.axis = 1.5, cex.lab = 1.5)
+dev.off()
+
+
+
+pdf("disc_min_tplap.pdf", width = 8, height = 6)
+plot(eps,disctplap, lwd = 2, main = "two-piece Laplace", type = "l",
+     xlab = expression(epsilon), ylab = "Discrepancy", cex.axis = 1.5, cex.lab = 1.5)
+dev.off()
+pdf("sdisc_min_tplap.pdf", width = 8, height = 6)
+plot(eps,stplap, lwd = 2, main = "two-piece Laplace", type = "l",
+     xlab = expression(epsilon), ylab = "Signed discrepancy", cex.axis = 1.5, cex.lab = 1.5)
+dev.off()
+pdf("prior_min_tplap.pdf", width = 8, height = 6)
+plot(eps,ptplap, lwd = 2, main = "two-piece Laplace", type = "l",
      xlab = expression(epsilon), ylab = "Prior", cex.axis = 1.5, cex.lab = 1.5)
 dev.off()
